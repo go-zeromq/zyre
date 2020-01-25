3ac902e5813b75f81eed9d795314ea8126be8ca8
Speak Your Code <speakyourcode.com@gmail.com>
2020-01-25 11:06:43 +0100

    zyre: zre msg

---
+++ msg.go
@@ -0,0 +1,225 @@
+// Copyright 2020 The go-zeromq Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package zyre provides reliable group messaging over local area networks.
+//
+// For more informations, see https://zeromq.org.
+package zyre
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
+
+	zmq "github.com/go-zeromq/zmq4"
+)
+
+const (
+	// Signature of the message
+	Signature uint16 = 0xAAA0 | 1
+	// Version number
+	Version byte = 2
+)
+
+// Message IDs
+const (
+	HelloID   uint8 = 1
+	WhisperID uint8 = 2
+	ShoutID   uint8 = 3
+	JoinID    uint8 = 4
+	LeaveID   uint8 = 5
+	PingID    uint8 = 6
+	PingOkID  uint8 = 7
+)
+
+// ZreMsg work with ZRE messages
+type ZreMsg struct {
+	routingID []byte            //  Routing_id from ROUTER, if any
+	id        uint8             //  ZreMsg message ID
+	sequence  uint16            //  Cyclic sequence number
+	Endpoint  string            //  Sender connect endpoint
+	Groups    []string          //  List of groups sender is in
+	Status    byte              //  Sender groups status value
+	Name      string            //  Sender public name
+	Headers   map[string]string //  Sender header properties
+	Content   []byte            //  Wrapped message content
+	Group     string            //  Name of Group
+}
+
+// NewZreMsg creates new ZreMsg message with messageID
+func NewZreMsg(id uint8) *ZreMsg {
+	msg := &ZreMsg{
+		id:      id,
+		Headers: make(map[string]string),
+	}
+	return msg
+}
+
+// Recv reads ZreMsg from ZMQ Socket
+func Recv(socket zmq.Socket) (*ZreMsg, error) {
+	input, err := socket.Recv()
+	msg := NewZreMsg(0)
+	if err != nil {
+		return msg, err
+	}
+	frames := input.Frames
+	if frames == nil { // FIXME: until pull request zmq4
+		return msg, errors.New("empty message")
+	}
+	// If we're reading from a ROUTER socket, get routingID
+	if socket.Type() == zmq.Router {
+		msg.routingID = frames[0]
+		frames = frames[1:]
+	}
+	var buffer *bytes.Buffer
+	buffer = bytes.NewBuffer(frames[0])
+	if signature := getNumber2(buffer); signature != Signature {
+		return msg, fmt.Errorf("invalid signature %X != %X", Signature, signature)
+	}
+	msg.id = getNumber1(buffer)
+	if version := getNumber1(buffer); version != Version {
+		return msg, errors.New("malformed version message")
+	}
+	msg.sequence = getNumber2(buffer)
+
+	switch msg.id {
+	case HelloID:
+		unpackHello(buffer, msg)
+	case WhisperID:
+		unpackContent(frames[1:], msg)
+	case ShoutID:
+		msg.Group = getString(buffer)
+		unpackContent(frames[1:], msg)
+	case JoinID, LeaveID:
+		msg.Group = getString(buffer)
+		msg.Status = getNumber1(buffer)
+	}
+
+	return msg, err
+}
+
+// Send sends ZreMsg through ZMQ socket.
+func Send(socket zmq.Socket, msg *ZreMsg) error {
+	var buffer bytes.Buffer
+	putNumber2(&buffer, Signature)
+	putNumber1(&buffer, msg.id)
+	putNumber1(&buffer, Version)
+	putNumber2(&buffer, msg.sequence)
+
+	switch msg.id {
+	case HelloID:
+		packHello(&buffer, msg)
+	case ShoutID:
+		putString(&buffer, msg.Group)
+	case JoinID, LeaveID:
+		putString(&buffer, msg.Group)
+		putNumber1(&buffer, msg.Status)
+	}
+
+	frames := [][]byte{buffer.Bytes()}
+	// If we're sending to a ROUTER, we send the routingID first
+	if socket.Type() == zmq.Router {
+		frames = append([][]byte{msg.routingID}, frames...)
+	}
+	if msg.id == WhisperID || msg.id == ShoutID {
+		frames = append(frames, msg.Content)
+	}
+	if socket.Type() == zmq.Router || msg.id == WhisperID ||
+		msg.id == ShoutID {
+		return socket.SendMulti(zmq.Msg{Frames: frames})
+	}
+	return socket.Send(zmq.Msg{Frames: frames})
+}
+
+func packHello(buffer *bytes.Buffer, msg *ZreMsg) {
+	putString(buffer, msg.Endpoint)
+	binary.Write(buffer, binary.BigEndian, uint32(len(msg.Groups)))
+	for _, val := range msg.Groups {
+		putLongString(buffer, val)
+	}
+	putNumber1(buffer, msg.Status)
+	putString(buffer, msg.Name)
+	binary.Write(buffer, binary.BigEndian, uint32(len(msg.Headers)))
+	for key, val := range msg.Headers {
+		putString(buffer, key)
+		putLongString(buffer, val)
+	}
+}
+
+func unpackHello(buffer *bytes.Buffer, msg *ZreMsg) {
+	msg.Endpoint = getString(buffer)
+	var groupsSize uint32
+	binary.Read(buffer, binary.BigEndian, &groupsSize)
+	for ; groupsSize != 0; groupsSize-- {
+		msg.Groups = append(msg.Groups, getLongString(buffer))
+	}
+	msg.Status = getNumber1(buffer)
+	msg.Name = getString(buffer)
+	var headersSize uint32
+	binary.Read(buffer, binary.BigEndian, &headersSize)
+	for ; headersSize != 0; headersSize-- {
+		key := getString(buffer)
+		val := getLongString(buffer)
+		msg.Headers[key] = val
+	}
+}
+
+func unpackContent(frames [][]byte, msg *ZreMsg) {
+	if 0 <= len(frames)-1 {
+		msg.Content = frames[0]
+	}
+}
+
+func putData(buffer *bytes.Buffer, data interface{}) {
+	binary.Write(buffer, binary.BigEndian, data)
+}
+
+func putNumber1(buffer *bytes.Buffer, number byte) {
+	putData(buffer, number)
+}
+
+func putNumber2(buffer *bytes.Buffer, number uint16) {
+	putData(buffer, number)
+}
+
+func putString(buffer *bytes.Buffer, str string) {
+	size := len(str)
+	putData(buffer, byte(size))
+	putData(buffer, []byte(str[0:size]))
+}
+
+func putLongString(buffer *bytes.Buffer, str string) {
+	size := len(str)
+	putData(buffer, uint32(size))
+	putData(buffer, []byte(str[0:size]))
+}
+
+func getNumber1(buffer *bytes.Buffer) byte {
+	var num byte
+	binary.Read(buffer, binary.BigEndian, &num)
+	return num
+}
+
+func getNumber2(buffer *bytes.Buffer) uint16 {
+	var num uint16
+	binary.Read(buffer, binary.BigEndian, &num)
+	return num
+}
+
+func getString(buffer *bytes.Buffer) string {
+	var size byte
+	binary.Read(buffer, binary.BigEndian, &size)
+	str := make([]byte, size)
+	binary.Read(buffer, binary.BigEndian, &str)
+	return string(str)
+}
+
+func getLongString(buffer *bytes.Buffer) string {
+	var size uint32
+	binary.Read(buffer, binary.BigEndian, &size)
+	str := make([]byte, size)
+	binary.Read(buffer, binary.BigEndian, &str)
+	return string(str)
+}
---
+++ msg_test.go
@@ -0,0 +1,213 @@
+// Copyright 2020 The go-zeromq Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package zyre provides reliable group messaging over local area networks.
+//
+// For more informations, see https://zeromq.org.
+package zyre
+
+import (
+	"bytes"
+	"context"
+	"testing"
+
+	zmq "github.com/go-zeromq/zmq4"
+)
+
+func TestNumber(t *testing.T) {
+	var buffer bytes.Buffer
+	putNumber1(&buffer, Version)
+	version := getNumber1(&buffer)
+	if version != Version {
+		t.Errorf("want %d, got %d", Version, version)
+	}
+	var givenSeq uint16 = 234
+	putNumber2(&buffer, givenSeq)
+	seq := getNumber2(&buffer)
+	if seq != givenSeq {
+		t.Errorf("want %d, got %d", givenSeq, seq)
+	}
+}
+
+func TestString(t *testing.T) {
+	var buffer bytes.Buffer
+	givenStr := "hello"
+	putString(&buffer, givenStr)
+	str := getString(&buffer)
+	if str != givenStr {
+		t.Errorf("want %s, got %s", givenStr, str)
+	}
+	givenStr = "bye"
+	putLongString(&buffer, givenStr)
+	str = getLongString(&buffer)
+	if str != givenStr {
+		t.Errorf("want %s, got %s", givenStr, str)
+	}
+}
+
+func TestZreMsg(t *testing.T) {
+	tests := []struct {
+		name string
+		id   uint8
+	}{
+		{name: "Hello", id: HelloID},
+		{name: "Whisper", id: WhisperID},
+		{name: "Shout", id: ShoutID},
+		{name: "Join", id: JoinID},
+		{name: "Leave", id: LeaveID},
+		{name: "Ping", id: PingID},
+		{name: "PingOK", id: PingOkID},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			endpoint := "inproc://selftest-" + tt.name
+			routingID := "Test"
+			dealer := zmq.NewDealer(ctx, zmq.WithID(zmq.SocketIdentity(routingID)))
+			defer dealer.Close()
+			checkError(dealer.Listen(endpoint), t)
+
+			router := zmq.NewRouter(ctx, zmq.WithID(zmq.SocketIdentity("router")))
+			defer router.Close()
+			checkError(router.Dial(endpoint), t)
+
+			// Create a ZreMsg message and send it through the wire
+			expectedMsg := NewZreMsg(tt.id)
+			expectedMsg.routingID = []byte(routingID)
+			expectedMsg.sequence = 123
+			expectedMsg.Endpoint = "Endpoint"
+			expectedMsg.Groups = []string{"Group1: A", "Group2: B"}
+			expectedMsg.Status = 123
+			expectedMsg.Name = "Name"
+			expectedMsg.Headers = map[string]string{"Header1": "1", "Header2": "2"}
+			expectedMsg.Group = "Group"
+			expectedMsg.Content = []byte("Some Content")
+
+			// dealer sends expectedMsg to router
+			checkError(Send(dealer, expectedMsg), t)
+			msg, err := Recv(router)
+			checkError(err, t)
+			checkMsg(msg, *expectedMsg, t)
+
+			// router sends back received msg to dealer
+			checkError(Send(router, msg), t)
+			msg, err = Recv(dealer)
+			checkError(err, t)
+			expectedMsg.routingID = []byte{}
+			checkMsg(msg, *expectedMsg, t)
+		})
+	}
+}
+
+func TestRecvError(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	endpoint := "inproc://selftest-error"
+	routingID := "Test"
+	dealer := zmq.NewDealer(ctx, zmq.WithID(zmq.SocketIdentity(routingID)))
+	defer dealer.Close()
+	checkError(dealer.Listen(endpoint), t)
+
+	router := zmq.NewRouter(ctx, zmq.WithID(zmq.SocketIdentity("router")))
+	defer router.Close()
+	checkError(router.Dial(endpoint), t)
+
+	// dealer sends Msg with no signature
+	dealer.Send(zmq.NewMsgString(""))
+	_, err := Recv(router)
+	if err == nil {
+		t.Error("error expected")
+	}
+	// dealer sends Msg with incorrect version
+	var buffer bytes.Buffer
+	putNumber2(&buffer, Signature)
+	putNumber1(&buffer, HelloID)
+	putNumber1(&buffer, 1)
+	dealer.Send(zmq.NewMsgFrom(buffer.Bytes()))
+	_, err = Recv(router)
+	if err == nil {
+		t.Error("error expected")
+	}
+
+	// socket Recv returns an error
+	cancel()
+	_, err = Recv(router)
+	if err == nil {
+		t.Error("error expected")
+	}
+}
+
+func checkMsg(msg *ZreMsg, expectedMsg ZreMsg, t *testing.T) {
+	switch msg.id {
+	case HelloID:
+		expectedMsg.id = HelloID
+		expectedMsg.Group = ""
+		expectedMsg.Content = []byte{}
+		checkZreMsg(msg, &expectedMsg, t)
+	case PingID, PingOkID:
+		expected := ZreMsg{routingID: expectedMsg.routingID, id: msg.id,
+			sequence: expectedMsg.sequence}
+		checkZreMsg(msg, &expected, t)
+	case JoinID, LeaveID:
+		expected := ZreMsg{routingID: expectedMsg.routingID, id: msg.id,
+			sequence: expectedMsg.sequence, Group: expectedMsg.Group,
+			Status: expectedMsg.Status}
+		checkZreMsg(msg, &expected, t)
+	case ShoutID:
+		expected := ZreMsg{routingID: expectedMsg.routingID, id: msg.id,
+			sequence: expectedMsg.sequence, Group: expectedMsg.Group,
+			Content: expectedMsg.Content}
+		checkZreMsg(msg, &expected, t)
+	case WhisperID:
+		expected := ZreMsg{routingID: expectedMsg.routingID, id: msg.id,
+			sequence: expectedMsg.sequence, Content: expectedMsg.Content}
+		checkZreMsg(msg, &expected, t)
+	}
+}
+
+func checkError(err error, t *testing.T) {
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func checkZreMsg(msg *ZreMsg, expected *ZreMsg, t *testing.T) {
+	if string(msg.routingID) != string(expected.routingID) {
+		t.Errorf("want %s, got %s", expected.routingID, msg.routingID)
+	}
+	if msg.id != expected.id {
+		t.Errorf("want %d, got %d", expected.id, msg.id)
+	}
+	if msg.sequence != expected.sequence {
+		t.Errorf("want %d, got %d", expected.sequence, msg.sequence)
+	}
+	if msg.Endpoint != expected.Endpoint {
+		t.Errorf("want %s, got %s", expected.Endpoint, msg.Endpoint)
+	}
+	for idx, str := range expected.Groups {
+		if msg.Groups[idx] != str {
+			t.Errorf("want %s, got %s", str, msg.Groups[idx])
+		}
+	}
+	if msg.Status != expected.Status {
+		t.Errorf("want %d, got %d", expected.Status, msg.Status)
+	}
+	if msg.Name != expected.Name {
+		t.Errorf("want %s, got %s", expected.Name, msg.Name)
+	}
+	if msg.Group != expected.Group {
+		t.Errorf("want %s, got %s", expected.Group, msg.Group)
+	}
+	if string(msg.Content) != string(expected.Content) {
+		t.Errorf("want %s, got %s", expected.Content, msg.Content)
+	}
+	for key, val := range expected.Headers {
+		if msg.Headers[key] != val {
+			t.Errorf("want %s, got %s", val, msg.Headers[key])
+		}
+	}
+}
